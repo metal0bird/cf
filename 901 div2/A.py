5ddc8286d3f4f2af3ff0f57624b63fdaea6e209b
Aman Gupta <aman1283871@gmail.com>
2023-10-01 17:36:03 +0530

    901 div2

---
+++ 901 div2/A.py
@@ -0,0 +1,10 @@
+N = int(input())
+for i in range(N):
+    limit , start , n = list(map(int,input().split()))
+    arr = list(map(int,input().split()))
+
+    sum = start
+    for a in arr:
+        sum+=min()
+
+    print(sum)
